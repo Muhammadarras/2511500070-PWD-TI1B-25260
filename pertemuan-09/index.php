462918f847ed837686df73f7f2cb7be7d5815220
Fahrel arifqi <2511500089@mahasiswa.atmaluhur.ac.id>
2025-11-26 20:22:23 -0800

    data

--- pertemuan-09/index.php
+++ pertemuan-09/index.php
@@ -201,7 +201,7 @@ endif;
   </main>
 
   <footer>
-    <p>&copy; muhammad arrasy [2511500070]</p>
+    <p>&copy; 2025 Yohanes Setiawan Japriadi [0344300002]</p>
   </footer>
 
   <script src="script.js"></script>
